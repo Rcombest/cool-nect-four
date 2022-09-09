2770b58bf01464c197d6c0cddb2596dbd95d3a2c
Ryan Combest <crcombest@gmail.com>
2022-09-09 12:45:34 -0500

    sets up CER's, init, and stubs up render

--- js/app.js
+++ js/app.js
@@ -8,10 +8,30 @@ let board, turn, winner
 
 /*------------------------ Cached Element References ------------------------*/
 
-
+const spaceEls = document.querySelectorAll('section > div')
+const messageEl = document.querySelector('#message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+function init() {
+  board = [
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null,
+    null, null, null, null, null, null, null
+  ]
+  turn = 1
+  winner = null
+  render() 
+}
+
+function render() {
+  
+}
